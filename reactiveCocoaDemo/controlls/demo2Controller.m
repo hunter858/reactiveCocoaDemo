89029b2522f5e7f90a9e00ee909398447531de55
pengchao <hunter858@sina.cn>
2017-08-09 13:08:49 +0800

    add file

--- reactiveCocoaDemo/controlls/demo2Controller.m
+++ reactiveCocoaDemo/controlls/demo2Controller.m
@@ -10,6 +10,10 @@
 #import "ReactiveObjC.h"
 @interface demo2Controller ()
 @property (strong, nonatomic) IBOutlet UIButton *createButton;
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel2;
+@property (strong, nonatomic) IBOutlet UIButton *notificationButton;
 
 @end
 
@@ -19,6 +23,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     [self demo4];
+    
+    [self demo5];
 }
 
 -(void)demo4{
@@ -26,7 +32,7 @@
     __weak typeof(self) weakself=self;
     [[self.createButton rac_signalForControlEvents:UIControlEventTouchUpInside]
          subscribeNext:^(id x) {
-             
+      
              [weakself createAlertView];
 
     }];
@@ -38,30 +44,38 @@
     //虽然 UIAlertView 在这里ios 9.0 之后 基本废弃，这里只做案例展示
     
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"RAC" message:@"RAC TEST" delegate:self cancelButtonTitle:@"cancel" otherButtonTitles:@"other", nil];
+    __weak typeof(self) weakself=self;
     [[self rac_signalForSelector:@selector(alertView:clickedButtonAtIndex:) fromProtocol:@protocol(UIAlertViewDelegate)] subscribeNext:^(RACTuple *tuple) {
-        NSLog(@"first:%@",tuple.first);
-        //tuple.first 是 UIAlertView 对象
+
+        NSLog(@"first:%@",tuple.first);//tuple.first 是 UIAlertView 对象
         
-        NSLog(@"second:%@",tuple.second);
-        //tuple.second 是 点击了第几个index的值
+        NSLog(@"second:%@",tuple.second);//tuple.second 是 点击了第几个index的值
+        
+        if([tuple.second isKindOfClass:[NSNumber class]]){
+            NSNumber *number = (NSNumber *) tuple.second;
+            
+            NSString *valueStr  =[NSString stringWithFormat:@"点击了第 %@ 个按钮",number];
+            weakself.resultLabel.text = valueStr;
+        }
         
     }];
     [alertView show];
 }
+
+-(void)demo5{
+    [[self.notificationButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+        
+        
+        
+    }];
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
